57c4dbad292d052d66607ef70d0fea7e094ebaf0
paulpietzko <paul.pietzko@icloud.com>
2024-12-03 09:32:22 +0100

    remove unused Monaco editor worker files and update worker URL handling

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,9 +1,39 @@
 // import 'monaco-editor/min/vs/editor/editor.main.css';
-import * as monaco from 'monaco-editor';
+import * as monaco from "monaco-editor";
 
 const winAny = window as any;
 winAny.appSourceCode ??= {};
 winAny.appSourceCode.activeMonacoEditor ??= activeMonacoEditor;
+
+// @ts-ignore
+self.MonacoEnvironment = {
+	getWorkerUrl: function (moduleId, label) {
+		if (label === 'json') {
+			return './json.worker.bundle.js';
+		}
+		if (label === 'css' || label === 'scss' || label === 'less') {
+			return './css.worker.bundle.js';
+		}
+		if (label === 'html' || label === 'handlebars' || label === 'razor') {
+			return './html.worker.bundle.js';
+		}
+		if (label === 'typescript' || label === 'javascript') {
+			return './ts.worker.bundle.js';
+		}
+		return './editor.worker.bundle.js';
+	}
+};
+
+interface MonacoEditorOptions {
+  value: string;
+  language: string;
+  automaticLayout: boolean;
+  theme: string;
+  lineNumbers: "on" | "off";
+  fontSize: number;
+  readOnly: boolean;
+  minimap: boolean;
+}
 
 function activeMonacoEditor({
   value,
@@ -14,24 +44,24 @@ function activeMonacoEditor({
   fontSize,
   readOnly,
   minimap,
-}: any) {
-  const container = document.getElementById('container');
-  
+}: MonacoEditorOptions) {
+  const container = document.getElementById("container");
+
   if (container) {
     monaco.editor.create(container, {
       value: value,
       language: language,
       theme: theme,
       automaticLayout: automaticLayout,
-      fontSize: parseInt(fontSize, 10),
+      fontSize: fontSize,
       lineNumbers: lineNumbers,
       minimap: { enabled: minimap },
       scrollBeyondLastLine: false,
       roundedSelection: false,
       readOnly: readOnly,
-      cursorStyle: 'line',
+      cursorStyle: "line",
     });
   } else {
-    console.error('Container element not found');
+    console.error("Container element not found");
   }
 }
--- src/ts/ts.worker.ts
+++
@@ -1,8 +0,0 @@
-import * as monaco from "monaco-editor";
-import * as tsWorker from "monaco-editor/esm/vs/language/typescript/ts.worker";
-
-self.onmessage = () => {
-  tsWorker.initialize(
-    (ctx: monaco.worker.IWorkerContext) => new tsWorker.TypeScriptWorker(ctx)
-  );
-};
