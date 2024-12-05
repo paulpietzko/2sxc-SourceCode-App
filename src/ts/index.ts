8f0f9f413b43f1437c426d0782f655e76efba0db
paulpietzko <paul.pietzko@icloud.com>
2024-12-05 13:42:23 +0100

    refactor: replace require.js with @monaco-editor/loader for Monaco editor initialization

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,4 +1,4 @@
-import type * as Monaco from 'monaco-editor';
+import loader from '@monaco-editor/loader';
 
 interface MonacoEditorOptions {
   value: string;
@@ -15,57 +15,35 @@ const winAny = window as any;
 winAny.appSourceCode ??= {};
 
 winAny.appSourceCode.initializeMonacoEditor = function (editorOptions: MonacoEditorOptions) {
-  // Load require.min.js dynamically
-  const script = document.createElement('script');
-  script.src = 'https://cdnjs.cloudflare.com/ajax/libs/require.js/2.3.6/require.min.js';
+loader.init().then(monaco => {
+  const {
+    value,
+    language,
+    automaticLayout,
+    theme,
+    lineNumbers,
+    fontSize,
+    readOnly,
+    minimap,
+  } = editorOptions;
 
-  script.onload = () => {
-    // Assign the require function to a private variable
-    const privateRequire = winAny.require;
+  const container = document.getElementById('container');
 
-    // Configure the privateRequire paths
-    privateRequire.config({
-      paths: {
-        vs: 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.52.0/min/vs',
-      },
+  if (container) {
+    monaco.editor.create(container, {
+      value: value,
+      language: language,
+      theme: theme,
+      automaticLayout: automaticLayout,
+      fontSize: fontSize,
+      lineNumbers: lineNumbers,
+      minimap: { enabled: minimap },
+      scrollBeyondLastLine: false,
+      roundedSelection: false,
+      readOnly: readOnly,
+      cursorStyle: 'line',
     });
-
-    // Use privateRequire to load the Monaco editor
-    privateRequire(['vs/editor/editor.main'], function (monaco: typeof Monaco) {
-      const {
-        value,
-        language,
-        automaticLayout,
-        theme,
-        lineNumbers,
-        fontSize,
-        readOnly,
-        minimap,
-      } = editorOptions;
-
-      const container = document.getElementById('container');
-
-      if (container) {
-        monaco.editor.create(container, {
-          value: value,
-          language: language,
-          theme: theme,
-          automaticLayout: automaticLayout,
-          fontSize: fontSize,
-          lineNumbers: lineNumbers,
-          minimap: { enabled: minimap },
-          scrollBeyondLastLine: false,
-          roundedSelection: false,
-          readOnly: readOnly,
-          cursorStyle: 'line',
-        });
-      } else {
-        console.error('Container element not found');
-      }
-    });
-  };
-  script.onerror = () => {
-    console.error('Failed to load require.min.js');
-  };
-  document.head.appendChild(script);
-};
+  } else {
+    console.error('Container element not found');
+  }
+});}
